b7af8c81bbc57bbccdbd3d8efb1f1ca1ba13e5e7
Morgan <morgan.facorat@gmail.com>
2019-09-13 21:51:36 +0200

    ajout category + relation

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -5,26 +5,65 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Post;
+use App\Entity\Category;
+use App\Entity\Comment;
 
 
-use Symfony\Bundle\FrameworkBundle\Tests\Fixtures\Validation\Article;class PostFixtures extends Fixture
+use Symfony\Bundle\FrameworkBundle\Tests\Fixtures\Validation\Article;
+class PostFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
-    {
-        for($i = 1 ; $i <= 10 ; $i++ ){
+    
+    { 
+        $faker = \Faker\Factory::create('fr_FR');
+       
+        for($i = 1 ; $i <= 3; $i++ ){
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     -> setDescription($faker->paragraph());
 
-            $post = new Post();
-            $post->setTitre("titre post $i")
-                 ->setContent("contenue de l'article $i")
-                 ->setImage("")
-                 ->setCreatAt(new \DateTime())
-                 ->setCreatBy("utilsateur n°$i");
-
-                 $manager->persist($post);
-
-
+                    $manager->persist($category);
         }
 
-        $manager->flush();
+
+
+                    for($j = 1 ; $j <= mt_rand(4,6) ; $j++ ){
+
+                   
+                    $contents = '<p>' .join($faker->paragraphs(5), '</p><p>').'</p>';
+                   
+                        $post = new Post();
+                        $post->setTitre($faker->sentence())
+                             ->setContent($contents)
+                             ->setDatePost($faker->dateTimeBetween('-6 months'))
+                             ->setUserCreator("utilsateur n°$i")
+                             ->setCategory($category);
+            
+                             $manager->persist($post);
+                            }
+
+
+                    for($k = 1; $k <= mt_rand(4, 10); $k++){
+
+                        $comment = new Comment();
+                        $content = '<p>' .join($faker->paragraphs(5), '</p><p>').'</p>';
+                    
+                        $days = (new \DateTime())->diff($post->getDatePost())->days;
+                       
+                   
+                            $comment->setAuthor($faker->name)
+                                    ->setContent($content)
+                                    ->setCreatedAt($faker->dateTimeBetween('-'.$days.'days'))
+                                    ->setArticle($post);
+
+                            $manager->persist($comment);
+
+                    }
+            
+                  
+                    $manager->flush();
+                    
+        }
+       
+      
     }
-}
